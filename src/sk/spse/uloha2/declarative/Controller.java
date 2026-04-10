81350806cd9a63bf7e4ddb0b05de2ec78a6eab7f
Cinnamon-bell <palinko.lompart@gmail.com>
2026-04-10 09:56:52 +0200

    css commit

--- src/sk/spse/uloha2/declarative/Controller.java
+++ src/sk/spse/uloha2/declarative/Controller.java
@@ -1,30 +1,43 @@
 package sk.spse.uloha2.declarative;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-/**
- * Controller pre FXML súbor – obsahuje logiku aplikácie
- */
 public class Controller {
+
     @FXML
-    public TextField userNameField;
-    public ToggleGroup Pohlavie;
-    public PasswordField hesloField;
+    private TextField userNameField;
+
+    @FXML
+    private ToggleGroup Pohlavie;
+
+    @FXML
+    private PasswordField hesloField;
 
     @FXML
     public void registruj() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Registrácia užívateľa");
-        alert.setTitle("Registrácia prebehla úspešne");
-        String pohlavie = ((RadioButton)Pohlavie.getSelectedToggle()).getText();
-        String message = String.format("Užívateľ %s (%s) s heslom %s bpol pridaný do systému", userNameField.getText(), pohlavie, hesloField.getText());
+        alert.setHeaderText("Registrácia prebehla úspešne");
+
+        String pohlavie = ((RadioButton) Pohlavie.getSelectedToggle()).getText();
+
+        String message = String.format(
+                "Užívateľ %s (%s) s heslom %s bol pridaný do systému",
+                userNameField.getText(),
+                pohlavie,
+                hesloField.getText()
+        );
+
         alert.setContentText(message);
         alert.showAndWait();
     }
 
     @FXML
-    private void Zatvor() {
-
+    public void close(ActionEvent keyEvent) {
+        Platform.exit();
     }
 }
--- src/sk/spse/uloha4/declarative/Controller.java
+++ src/sk/spse/uloha4/declarative/Controller.java
@@ -1,30 +1,63 @@
-/*package sk.spse.uloha4.declarative;
+package sk.spse.uloha4.declarative;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.VBox;;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
 
-/**
- * Controller pre FXML súbor – obsahuje logiku aplikácie
+import java.util.Locale;
+import java.util.Random;
 
 public class Controller {
 
     public VBox vbox1;
+    public VBox vbox2;
+    public VBox vbox3;
+    public VBox vbox4;
 
-    public static String getRandomColor(){
+    public ImageView img1;
+    public ImageView img2;
+    public ImageView img3;
+    public ImageView img4;
+
+    private static final Random rand = new Random();
+
+    public static String getRandomColor() {
         int r = rand.nextInt(256);
         int g = rand.nextInt(256);
         int b = rand.nextInt(256);
         return String.format("#%02X%02X%02X", r, g, b);
     }
 
+    public static String getRandomOpaque() {
+        double x = rand.nextDouble();
+        return String.format(Locale.ROOT, "%.2f", x);
+    }
+
+    public static double getRandomScale() {
+        return 0.5 + rand.nextDouble();
+    }
+
+    public static double getRandomRotate() {
+        return rand.nextInt(360);
+    }
+
     @FXML
-    private void incrementCounter() {
-        private void randomize() {
-            vbox1.setStyle("-fx-background-color: " + getRandomColor());
-            vbox2.setStyle("-fx-background-color: " + getRandomColor());
-            vbox3.setStyle("-fx-background-color: " + getRandomColor());
-            vbox4.setStyle("-fx-background-color: " + getRandomColor());
-        }
+    private void randomize() {
+        vbox1.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox2.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox3.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+        vbox4.setStyle("-fx-background-color: " + getRandomColor() + "; -fx-opacity: " + getRandomOpaque());
+
+        img4.setStyle("-fx-rotate: " + getRandomRotate());
+        img2.setStyle("-fx-scale-x: " + getRandomScale());
+        img3.setStyle("-fx-scale-y: " + getRandomScale());
+        img1.setStyle("-fx-rotate: " + getRandomRotate());
+    }
+
+    @FXML
+    public void close(ActionEvent keyEvent) {
+        Platform.exit();
     }
 }
-*/
